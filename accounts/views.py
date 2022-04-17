b4e75be6cd0ab2345311421f7ed01e32ba4815f1
George Muteti <thomas.muteti@gmail.com>
2022-04-17 20:06:27 +0200

    Integrating templates.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from .forms import UserRegistration, UserEditForm
+
+
+def dashboard(request):
+    return render(request, 'dashboard/index.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegistration(request.POST or None)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.set_password(
+                form.cleaned_data.get('password')
+            )
+            new_user.save()
+            return render(request, 'authapp/register_done.html')
+    else:
+        form = UserRegistration()
+
+    context = {
+        "form": form
+    }
+
+    return render(request, 'authapp/register.html', context=context)
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user,
+                                 data=request.POST)
+        if user_form.is_valid():
+            user_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+    context = {
+        'form': user_form,
+    }
+    return render(request, 'authapp/edit.html', context=context)
--- majiweb/settings.py
+++ majiweb/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -75,13 +76,16 @@ WSGI_APPLICATION = 'majiweb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'majiweb',
+      'USER':'postgres',
+      'PASSWORD':'postgres',
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -119,7 +123,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static", "static_root")
+
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static", "our_static"),
+
+)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'accounts:dashboard'
